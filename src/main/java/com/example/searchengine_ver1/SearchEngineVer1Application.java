b26887da5b57680e96be62063d674ef66f7352ac
Patriciadah <braica.patricia@yahoo.com>
2025-03-21 21:23:24 +0200

    refactor(all):base for project

---
+++ src/main/java/com/example/searchengine_ver1/SearchEngineVer1Application.java
@@ -0,0 +1,13 @@
+package com.example.searchengine_ver1;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SearchEngineVer1Application {
+
+    public static void main(String[] args) {
+        SpringApplication.run(SearchEngineVer1Application.class, args);
+    }
+
+}
